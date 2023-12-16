086e90d5c688051d304ca2b0cc12230e17099a8d
creme332 <65414576+creme332@users.noreply.github.com>
2023-12-16 15:20:16 +0400

    add ability to specify head tags in template

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -2,25 +2,34 @@
 
 trait Controller
 {
+
+
     /**
-     * Displays view
+     * Renders a view
+     *
+     * @param  mixed $filename File name of view file in `views` folder.
+     * @param  mixed $data  Additional data defined in the view.
+     * @param  mixed $template_title Page title. Default value is `lamp`.
+     * @param  mixed $template_tags Optional tags to be included in head. Examples can be
+     * script tags and links to stylesheets.
+     *
+     * ! Any links used inside $template_tags should be absolute (include ROOT).
+     * @return void
      */
-    public function view($name, $data = [])
+    public function view(string $filename, array $data = [], string $template_title = 'lamp', string $template_tags = '')
     {
         if (!empty($data)) {
             extract($data);
         }
-        $filename = '../src/views/' . ucfirst($name) . '.php';
-        $content = ''; // html content for template
-        $title = ucfirst($name); // title for template
-
+        $template_filename = '../src/views/' . ucfirst($filename) . '.php';
+        $template_content = ''; // html content for template
         ob_start();
-        if (file_exists($filename)) {
-            include($filename);
+        if (file_exists($template_filename)) {
+            include($template_filename);
         } else {
             include('../src/views/404.php');
         }
-        $content = ob_get_contents();
+        $template_content = ob_get_contents();
         ob_end_clean();
 
         $template_filename = "../src/views/Template.php";
--- src/views/Template.php
+++ src/views/Template.php
@@ -8,16 +8,57 @@
     <meta name="description" content="LAMP template project" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" />
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐞</text></svg>" />
-    <title><?= $title ?> | LAMP</title>
+    <?= $template_tags ?>
+    <title><?= $template_title ?></title>
 </head>
 
 <body>
-    <main id="app" class="container">
-        <!-- <h1><?= $title ?></h1> -->
-        <?= $content ?>
-    </main>
+    <nav class="container-fluid">
+        <ul>
+            <li>
+                <a href="<?= ROOT ?>/" class="contrast"><strong>lamp</strong></a>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <details role="list" dir="rtl">
+                    <summary aria-haspopup="listbox" role="link" class="secondary">Theme</summary>
+                    <ul role="listbox">
+                        <li><a href="#" data-theme-switcher="auto">Auto</a></li>
+                        <li><a href="#" data-theme-switcher="light">Light</a></li>
+                        <li><a href="#" data-theme-switcher="dark">Dark</a></li>
+                    </ul>
+                </details>
+            </li>
+            <li>
+                <details role="list" dir="rtl">
+                    <summary aria-haspopup="listbox" role="link" class="secondary">Examples (v1)</summary>
+                    <ul role="listbox">
+                        <li><a href="../v1-preview/">Preview</a></li>
+                        <li><a href="../v1-preview-rtl/">Right-to-left</a></li>
+                        <li><a href="../v1-classless/">Classless</a></li>
+                        <li><a href="../v1-basic-template/">Basic template</a></li>
+                        <li><a href="../v1-company/">Company</a></li>
+                        <li><a href="../v1-google-amp/">Google Amp</a></li>
+                        <li><a href="../v1-sign-in/">Sign in</a></li>
+                        <li><a href="../v1-bootstrap-grid/">Bootstrap grid</a></li>
+                    </ul>
+                </details>
+            </li>
+        </ul>
+    </nav>
+
+    <?= $template_content ?>
+
+    <footer class="container-fluid">
+        <small>Built with <a href="https://picocss.com" class="secondary">Pico</a> •
+            <a href="https://github.com/picocss/examples/tree/master/v1-sign-in/" class="secondary">Source code</a></small>
+    </footer>
+
+    <!-- Minimal theme switcher -->
+    <script src="js/minimal-theme-switcher.js"></script>
 </body>
 
 </html>
