fb41cf030954da60f8802b8f45dedde2b1040af3
creme332 <65414576+creme332@users.noreply.github.com>
2023-12-16 19:15:28 +0400

    add global style to template

--- src/views/Template.php
+++ src/views/Template.php
@@ -9,6 +9,8 @@
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" />
+    <link rel="stylesheet" href="<?= ROOT ?>/styles/global.css" />
+
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐞</text></svg>" />
     <?= $template_tags ?>
     <title><?= $template_title ?></title>
