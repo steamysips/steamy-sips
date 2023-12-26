1581a6fb3c10601c65fbd8e21ad2495b150035f9
creme332 <65414576+creme332@users.noreply.github.com>
2023-12-26 18:47:54 +0400

    add new libs, use icons in header

--- src/views/Template.php
+++ src/views/Template.php
@@ -12,14 +12,22 @@
 
     <!-- start of styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" integrity="sha512-1cK78a1o+ht2JcaW6g8OXYwqpev9+6GqOkz9xmBN9iUUhIndKtxwILGWYOSibOKjLsEdjyjZvYDq/cZwNeak0w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?= ROOT ?>/styles/global.css" />
     <!-- end of styles -->
+
+    <!-- start of scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js" integrity="sha512-A7AYk1fGKX6S2SsHywmPkrnzTZHrgiVT7GcQkLGDe2ev0aWb8zejytzS8wjo7PGEXKqJOrjQ4oORtnimIRZBtw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js"></script>
+    <!-- end of scripts -->
 
     <?= $template_tags ?>
     <title><?= $template_title ?></title>
 </head>
 
 <body>
+
     <nav class="container-fluid">
         <ul>
             <li>
@@ -35,15 +43,25 @@
             </li>
         </ul>
         <ul>
-            <li><a href="<?= ROOT ?>/shop" class="contrast">
+            <li>
+                <a href="<?= ROOT ?>/shop" class="contrast">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
                         <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
                         <path d="M17 17h-11v-14h-2" />
                         <path d="M6 5l14 1l-1 7h-13" />
-                    </svg></a></li>
-            <li><a href="<?= ROOT ?>/login">Login</a></li>
+                    </svg>
+                </a>
+            </li>
+            <li>
+                <a href="<?= ROOT ?>/login" class="contrast"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                        <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                    </svg>
+                </a>
+            </li>
 
             <li>
                 <details role="list" dir="rtl">
@@ -64,9 +82,6 @@
         <small>Built by <a href="https://github.com/creme332" class="secondary">creme332</a> •
             <a href="https://github.com/creme332/skeleton/" class="secondary">Source code</a></small>
     </footer>
-
-    <!-- Minimal theme switcher -->
-    <script src="js/minimal-theme-switcher.js"></script>
 </body>
 
 </html>
