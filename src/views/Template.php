f897f0ae1ad3f5590bf9d92a0682303fd80be9fe
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-11 19:13:24 +0400

    update header and footer

--- src/views/Template.php
+++ src/views/Template.php
@@ -61,18 +61,19 @@
         </li>
     </ul>
     <ul>
+        <a href="<?= ROOT ?>/shop" class="secondary">
+            Shop
+        </a>
         <li>
-            <a href="<?= ROOT ?>/shop" class="contrast">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="24"
-                     height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                     stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"/>
-                    <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"/>
-                    <path d="M17 17h-11v-14h-2"/>
-                    <path d="M6 5l14 1l-1 7h-13"/>
-                </svg>
-            </a>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="24"
+                 height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                 stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"/>
+                <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"/>
+                <path d="M17 17h-11v-14h-2"/>
+                <path d="M6 5l14 1l-1 7h-13"/>
+            </svg>
         </li>
         <li>
             <a href="<?= ROOT ?>/login" class="contrast">
@@ -101,9 +102,10 @@
 
 <?= $template_content ?>
 
-<footer class="container-fluid">
-    <small>Built by <a href="https://github.com/creme332" class="secondary">creme332</a> •
-        <a href="https://github.com/creme332/skeleton/" class="secondary">Source code</a></small>
+<footer id="page-footer" class="container-fluid">
+    <small class="secondary">
+        © 2024 Steamy Sips Café
+    </small>
 </footer>
 </body>
 
