32cc3c788f68d05a16c2b3245c0acf122c7d0e01
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-11 18:35:40 +0400

    add svg wave

--- src/views/Home.php
+++ src/views/Home.php
@@ -1,3 +1,10 @@
+<svg data-aos="fade-right"
+     data-aos-offset="300"
+     data-aos-easing="ease-in-sine" class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path fill="#f3f4f5" fill-opacity="1"
+          d="M0,128L17.1,133.3C34.3,139,69,149,103,176C137.1,203,171,245,206,261.3C240,277,274,267,309,245.3C342.9,224,377,192,411,197.3C445.7,203,480,245,514,245.3C548.6,245,583,203,617,160C651.4,117,686,75,720,64C754.3,53,789,75,823,106.7C857.1,139,891,181,926,192C960,203,994,181,1029,170.7C1062.9,160,1097,160,1131,170.7C1165.7,181,1200,203,1234,192C1268.6,181,1303,139,1337,101.3C1371.4,64,1406,32,1423,16L1440,0L1440,320L1422.9,320C1405.7,320,1371,320,1337,320C1302.9,320,1269,320,1234,320C1200,320,1166,320,1131,320C1097.1,320,1063,320,1029,320C994.3,320,960,320,926,320C891.4,320,857,320,823,320C788.6,320,754,320,720,320C685.7,320,651,320,617,320C582.9,320,549,320,514,320C480,320,446,320,411,320C377.1,320,343,320,309,320C274.3,320,240,320,206,320C171.4,320,137,320,103,320C68.6,320,34,320,17,320L0,320Z"></path>
+</svg>
+
 <main class="container">
     <section id="introduction" class="grid">
         <div>
@@ -9,18 +16,19 @@
                 delights, where the perfect brew meets community and conversation. From the first scent of freshly
                 ground beans to the moment your cup touches your lips, we craft each coffee with passion and
                 precision.</p>
-            <div>
-                <button>Shop now</button>
-            </div>
+            <!--            <div>-->
+            <!--                <button class="contrast">Shop now</button>-->
+            <!--            </div>-->
         </div>
         <div>
-            <img src="https://images.squarespace-cdn.com/content/v1/560bddaae4b014538d6cc48a/1680021010200-O38HVLE98JXZC6AYZK0N/ethiopia.png"
-                 alt="Image of coffee beans">
+            <img src="<?= ROOT ?>/assets/img/ethiopia.png"
+                 height="200" width="500"
+                 alt="Coffee beans in a white bowl">
         </div>
     </section>
 
     <section data-aos="fade-left" id="about-us">
-        <h2>About Us</h2>
+<!--        <h2>About Us</h2>-->
         <div class="grid">
             <article>
                 <header>
@@ -196,6 +204,8 @@
         </article>
     </section>
 </main>
+
+
 <script defer src="<?= ROOT ?>/js/minimal-theme-switcher.js"></script>
 
 <script>
