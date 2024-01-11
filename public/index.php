478cab4be106633cb6e1cc2215df7de1cbf618aa
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-11 21:31:29 +0400

    specify utf-8 encoding

--- public/index.php
+++ public/index.php
@@ -1,7 +1,10 @@
 <?php
+/* https://phptherightway.com/#utf-8-at-the-php-level */
+mb_internal_encoding("UTF-8");
+mb_http_output("UTF-8");
 
 session_start();
 require '../src/core/init.php';
 
-$app =  new App();
+$app = new App();
 $app->loadController();
