69e08c1832670ff4f98ac3968781cd70038db4c4
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-15 13:03:49 +0400

    move initialization stuffs to init.php file

--- public/index.php
+++ public/index.php
@@ -1,10 +1,6 @@
 <?php
-/* https://phptherightway.com/#utf-8-at-the-php-level */
-mb_internal_encoding("UTF-8");
-mb_http_output("UTF-8");
 
-session_start();
-require '../src/core/init.php';
+require '../src/core/init.php'; // initialize app
 
-$app = new App();
+$app = new Steamy\Core\App();
 $app->loadController();
--- src/core/init.php
+++ src/core/init.php
@@ -1,6 +1,13 @@
 <?php
 
-require '../vendor/autoload.php';
+/* Define character encoding
+  Reference: https://phptherightway.com/#utf-8-at-the-php-level
+*/
+mb_internal_encoding("UTF-8");
+mb_http_output("UTF-8");
+
+session_start(); // start a new session
+
+require '../vendor/autoload.php'; // autoload classes
 require 'config.php'; // define configurations for database and root path
 require 'functions.php'; // define global helper functions
-require 'App.php';
