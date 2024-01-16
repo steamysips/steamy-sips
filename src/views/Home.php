4c8bef8575c6c67a1250c2cd6e04994a12b8ec66
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-16 14:51:12 +0400

    revamp about-us section

--- src/views/Home.php
+++ src/views/Home.php
@@ -30,41 +30,21 @@
         <div class="grid">
             <article>
                 <header>
-                    <h5>Craft Your Blend</h5>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart" width="24"
-                         height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                         stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"/>
-                    </svg>
+                    <h5 data-theme="dark">Craft Your Blend</h5>
                 </header>
                 <small>Personalize your coffee blend, selecting from diverse beans, roasts, and flavors for a truly
                     unique brew tailored to your preferences.</small>
             </article>
             <article>
                 <header>
-                    <h5>Tasting Experiences</h5>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-thumb-up" width="24"
-                         height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                         stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M7 11v8a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1v-7a1 1 0 0 1 1 -1h3a4 4 0 0 0 4 -4v-1a2 2 0 0 1 4 0v5h3a2 2 0 0 1 2 2l-1 5a2 3 0 0 1 -2 2h-7a3 3 0 0 1 -3 -3"/>
-                    </svg>
+                    <h5 data-theme="dark">Tasting Experiences</h5>
                 </header>
                 <small>Join our guided tasting sessions exploring various coffee origins, roasting methods, and flavor
                     profiles, elevating your understanding and enjoyment of coffee.</small>
             </article>
             <article>
                 <header>
-                    <h5>Ethical Sourcing</h5>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-seeding" width="24"
-                         height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                         stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M12 10a6 6 0 0 0 -6 -6h-3v2a6 6 0 0 0 6 6h3"/>
-                        <path d="M12 14a6 6 0 0 1 6 -6h3v1a6 6 0 0 1 -6 6h-3"/>
-                        <path d="M12 20l0 -10"/>
-                    </svg>
+                    <h5 data-theme="dark">Ethical Sourcing</h5>
                 </header>
                 <small>Embrace our commitment to ethical bean sourcing and sustainable practices, supporting fair trade,
                     eco-friendly packaging, and recycling initiatives.</small>
