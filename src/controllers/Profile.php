1ac4f102c7fdf7266324ef5b46556e7210754003
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-17 14:21:38 +0400

    redirect user to login page if not logged in

--- src/controllers/Profile.php
+++ src/controllers/Profile.php
@@ -3,13 +3,23 @@
 namespace Steamy\Controller;
 
 use Steamy\Core\Controller;
-
+use Steamy\Model\User;
 class Profile
 {
     use Controller;
 
     public function index(): void
     {
+        // if user is not signed in, redirect to login page
+        session_regenerate_id();
+        if (!array_key_exists('user', $_SESSION) || !isset($_SESSION['user'])) {
+            redirect('login');
+        }
+
+        // fetch user details from database
+        $current_user = new User();
+
+        // display user profile
         $this->view('Profile');
     }
 }
