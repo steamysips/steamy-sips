5a03f501424fc5795596b8a36c348033b82e51ec
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-22 17:07:40 +0400

    implement nested comment system in php

---
+++ src/controllers/Product.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Steamy\Controller;
+
+use Steamy\Core\Controller;
+use Steamy\Core\Utility;
+
+/**
+ * Displays product page when URL follows format `/shop/products/<number>`.
+ */
+class Product
+{
+    use Controller;
+
+    public function index(): void
+    {
+        // get product id from URL
+        $product_id = Utility::splitURL()[2];
+
+        // fetch product data from database
+        $data["product"] = (object)[
+            'name' => 'Espresso',
+            'description' => 'Personalize your coffee blend, selecting from diverse beans, roasts, and flavors
+                 for a truly unique brew tailored to your preferences.',
+            'rating' => 3.1
+        ];
+
+        if ($data["product"] == null) {
+            // if product was not found, display error page
+            $this->view(
+                '404',
+                template_title: 'Not found'
+            );
+            return;
+        }
+
+        // get average rating of product (calculated in sql)
+        $data["product"]->avg_rating = 3.1;
+
+        // get all reviews for product
+        $data["product"]->reviews = array_fill(
+            0,
+            10,
+            (object)[
+                'date' => 'today',
+                'author' => 'User123',
+                'text' => 'This is a comment.',
+                'children' => [
+                    (object)[
+                        'author' => 'User123',
+                        'text' => 'This is a comment.',
+                        'children' => [
+                            (object)[
+                                'author' => 'User123',
+                                'text' => 'This is a comment.',
+                                'children' => [
+                                    (object)[
+                                        'author' => 'User123',
+                                        'text' => 'This is a comment.',
+                                        'children' => []
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        );
+
+        $this->view(
+            'Product',
+            $data,
+            'Product name'
+        );
+    }
+}
--- src/views/Product.php
+++ src/views/Product.php
@@ -1,94 +1,69 @@
+<?php
+/**
+ * TODO: Update me after database creation
+ * @var $product mixed product information
+ */
+
+?>
 <main class="container">
     <div class="grid">
         <img src="<?= ROOT ?>/assets/img/login-milkshake.avif" alt="">
         <div class="">
             <hgroup>
-                <h2>Espresso</h2>
-                <h3>Average rating: 3.1</h3>
+                <h2><?= $product->name ?></h2>
+                <h3>Average rating: <?= $product->avg_rating ?></h3>
             </hgroup>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
+            <p>
+                <?= $product->description ?>
             </p>
             <button>Add to cart</button>
         </div>
     </div>
 
-    <h2>Customer Reviews</h2>
+    <h2>Customer Reviews (<?= count($product->reviews) ?>)</h2>
     <form action="" class="grid">
         <label>
             <input placeholder="Write a new review" type="text">
         </label>
         <button type="submit">Submit</button>
     </form>
-    <div id="card_01_comments">
+    <div id="reviews">
+        <ul>
 
+            <?php
+            function recurse($review)
+            {
+                echo <<<EOL
+                <li>
+                <article>
+                    <h5>$review->author @ $review->date</h5>
+                    <p>$review->text</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-message-reply"
+                     width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                      stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"
+                       fill="none"/><path d="M18 4a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12z" />
+                       <path d="M11 8l-3 3l3 3" /><path d="M16 11h-8" /></svg>
+                </article>
+                EOL;
+
+                // print child comments if any
+                if (isset($review->children)) {
+                    foreach ($review->children as $child_comment) {
+                        echo "<ul>";
+                        recurse($child_comment);
+                        echo "</ul>";
+                    }
+                }
+
+                echo "</li>";
+            }
+
+            // print top-level comments
+            foreach ($product->reviews as $review) {
+                recurse($review);
+            }
+            ?>
+        </ul>
 
     </div>
 </main>
-
-<style>
-  ul > li {
-    list-style-type: none;
-  }
-
-  li > article {
-    margin: 0;
-  }
-
-  ul > ul {
-    padding-left: 55px;
-  }
-</style>
-
-
-<script>
-  // ! JAVASCRIPT IS NOT NEEDED
-  // THIS CAN BE DONE IN PHP on server-side
-  const comments = [
-    {
-      text: "this is the first comment",
-      children: [
-        {
-          text: "this is the first subcomment",
-          children: "",
-        },
-        {
-          text: "this is the second subcomment",
-          children: [
-            {
-              text: "third level comment",
-              children: [{text: "fourth level"}],
-            },
-          ],
-        },
-      ],
-    },
-    {text: "this is the second comment"},
-  ];
-
-  let commentary = "<ul>";
-
-  function getCommentHTML() {
-
-  }
-
-  function recurse(comment) {
-    commentary += `<li><article>${comment.text}</article></li>`;
-    if (comment.children) {
-      commentary += "<ul>";
-      for (i in comment.children) {
-        recurse(comment.children[i]);
-      }
-      commentary += "</ul>";
-    }
-  }
-
-  for (comment in comments) {
-    recurse(comments[comment]);
-  }
-
-  document.querySelector("#card_01_comments").innerHTML += commentary + "</ul>";
-</script>
