f28dc3ea30c2a6c4e605db90677be6981df7a1c3
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-26 11:55:25 +0400

    add charts to dashboard

--- src/controllers/Dashboard.php
+++ src/controllers/Dashboard.php
@@ -20,10 +20,18 @@ class Dashboard
 
         $data['users'] = (new User())->all();
 
+
+        $script_tags = <<<EOL
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js"
+            integrity="sha512-ZwR1/gSZM3ai6vCdI+LVF1zSq/5HznD3ZSTk7kajkaj4D292NLuduDCO1c/NT8Id+jE58KYLKT7hXnbtryGmMg=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        EOL;
+
         $this->view(
             'Dashboard',
             $data,
             'Dashboard',
+            template_tags: $script_tags
         );
     }
 }
--- src/views/Dashboard.php
+++ src/views/Dashboard.php
@@ -5,10 +5,36 @@
  *     password:string
  * } $users Array of all users as fetched from database
  */
+
 ?>
 <main class="container">
     <h1>Dashboard</h1>
+    <div class="grid">
+        <article>
+            <h6>Total sales</h6>
+            <h3>Rs 9999</h3>
+        </article>
+        <article>
+            <h6>Total orders</h6>
+            <h3>9999</h3></article>
+        <article>
+            <h6>Total customers</h6>
+            <h3>9999</h3>
+        </article>
 
+    </div>
+    <h2>Sales revenue</h2>
+
+    <div style="width: 800px;">
+        <canvas id="acquisitions"></canvas>
+    </div>
+
+    <h2>Growth</h2>
+    <div style="width: 800px;">
+        <canvas id="aa"></canvas>
+    </div>
+
+    <h2>Recent orders</h2>
     <table>
         <thead>
         <tr>
@@ -34,3 +60,55 @@
         <Button>Create new user</Button>
     </a>
 </main>
+
+<script>
+  (async function() {
+    const data = [
+      {year: 2010, count: 10},
+      {year: 2011, count: 20},
+      {year: 2012, count: 15},
+      {year: 2013, count: 25},
+      {year: 2014, count: 22},
+      {year: 2015, count: 30},
+      {year: 2016, count: 28},
+    ];
+
+    new Chart(
+        document.getElementById("acquisitions"),
+        {
+          type: "bar",
+          data: {
+            labels: data.map(row => row.year),
+            datasets: [
+              {
+                label: "Acquisitions by year",
+                data: data.map(row => row.count),
+              },
+            ],
+          },
+        },
+    );
+  })();
+
+  (async function() {
+    const labels = ["January", "February", "March", "April", "May", "June", "July"];
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: "Sales Revenue",
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: "rgb(75, 192, 192)",
+          tension: 0.1,
+        }],
+    };
+    const config = {
+      type: "line",
+      data: data,
+    };
+    new Chart(
+        document.getElementById("aa"), config,
+    );
+  })();
+</script>
