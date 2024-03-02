b3d61081d557a682cbd3d7cae732cbfa24404129
creme332 <65414576+creme332@users.noreply.github.com>
2024-03-02 17:05:01 +0400

    create abstract `User` class

--- src/models/User.php
+++ src/models/User.php
@@ -4,72 +4,107 @@ namespace Steamy\Model;
 
 use Steamy\Core\Model;
 
-class User
+abstract class User
 {
     use Model;
 
     protected string $table = 'user';
-    private string $name;
-    private string $password;
+    protected int $user_id = -1;
+    protected string $email = "";
+    protected string $first_name = "";
+    protected string $last_name = "";
+    protected string $password = "";
+    protected string $phone_no = "";
 
-    public function __construct(string $name = '', string $password = '')
-    {
-        $this->name = $name;
-        $this->password = $password;
+    public function __construct(
+        string $email,
+        string $first_name,
+        string $last_name,
+        string $password,
+        string $phone_no
+    ) {
+        $this->email = $email;
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+        $this->password = password_hash($password, PASSWORD_BCRYPT);;
+        $this->phone_no = $phone_no;
     }
 
-    // sanitize
-
-    /**
-     * Validates user properties
-     * @param $data array data with name and password properties
-     * @return bool True if data passes all checks
-     */
-    public function validate(array $data): bool
+    public function toArray(): array
     {
-        $this->errors = [];
-
-        if (empty($data['name'])) {
-            $this->errors['name'] = "Name is required";
-        }
-
-        if (strlen($data['name']) < 5) {
-            $this->errors['name'] = "Name too short";
-        }
-
-        if (empty($data['password'])) {
-            $this->errors['password'] = "Password is required";
-        }
-
-        if (strlen($data['password']) < 5) {
-            $this->errors['password'] = "Password too short";
-        }
-
-        if (array_key_exists('confirmPassword', $data) && $data['password'] != $data['confirmPassword']) {
-            $this->errors['confirmPassword'] = "Passwords do not match";
-        }
-
-        return (empty($this->errors));
+        return
+            (array)[
+                'user_id' => $this->user_id,
+                'email' => $this->email,
+                'first_name' => $this->first_name,
+                'password' => $this->password,
+                'phone_no' => $this->phone_no,
+                'last_name' => $this->last_name
+            ];
     }
 
-    // ! Override insert and use bcrypt
-    public function getName(): string
+    public function validate()
     {
-        return $this->name;
+        // validate current object
     }
 
-    public function setName(string $name): void
+    public function save()
     {
-        $this->name = $name;
+        // if errors present exit
+        if (count($this->validate()) > 0) {
+            return;
+        }
+
+        // else insert current object to database
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function setFirstName(string $first_name): void
+    {
+        $this->first_name = $first_name;
+    }
+
+    public function setLastName(string $last_name): void
+    {
+        $this->last_name = $last_name;
+    }
+
+    public function setPassword(string $original_password): void
+    {
+        $this->password = password_hash($original_password, PASSWORD_BCRYPT);
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->first_name;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->last_name;
+    }
+
+    public function getPhoneNo(): string
+    {
+        return $this->phone_no;
+    }
+
+    public function getFullName(): string
+    {
+        return "$this->first_name $this->last_name";
     }
 
     public function getPassword(): string
     {
         return $this->password;
-    }
-
-    public function setPassword(string $password): void
-    {
-        $this->password = $password;
     }
 }
