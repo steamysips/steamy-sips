0ce51e92f6fff60f4af12a6a303d7a3124810615
creme332 <65414576+creme332@users.noreply.github.com>
2024-03-03 12:56:41 +0400

    add optional `table_name` parameter to all functions

--- src/core/Model.php
+++ src/core/Model.php
@@ -2,6 +2,8 @@
 
 namespace Steamy\Core;
 
+use stdClass;
+
 trait Model
 {
     use Database;
@@ -11,11 +13,13 @@ trait Model
 
     /**
      * Returns all records from a table, ignoring $limit.
+     * @param string $table_name Name of table. Defaults to $this->table.
      * @return false|array
      */
-    protected function all(): false|array
+    protected function all(string $table_name = ""): false|array
     {
-        $query = "SELECT * FROM $this->table";
+        $table_name = empty($table_name) ? $this->table : $table_name;
+        $query = "SELECT * FROM $table_name";
         return self::query($query);
     }
 
@@ -23,15 +27,18 @@ trait Model
      * Build and execute a SELECT query based on the provided conditions.
      *
      * @param array $data An associative array representing the conditions for the WHERE clause.
+     * @param string $table_name Name of table. Defaults to $this->table.
      * @param array $data_not An associative array representing the conditions for the NOT part of the WHERE clause.
      *
      * @return false|array Returns false if the query execution fails, otherwise returns an array of the query results.
      */
-    protected function where(array $data, array $data_not = []): false|array
+    protected function where(array $data, string $table_name = "", array $data_not = []): false|array
     {
+        $table_name = empty($table_name) ? $this->table : $table_name;
+
         $keys = array_keys($data);
         $keys_not = array_keys($data_not);
-        $query = "SELECT * FROM $this->table WHERE ";
+        $query = "SELECT * FROM $table_name WHERE ";
 
         foreach ($keys as $key) {
             $query .= $key . " = :" . $key . " && ";
@@ -50,11 +57,12 @@ trait Model
     /**
      * Returns the first result from an executed SELECT query
      * @param array $data
-     * @return \stdClass|null
+     * @param string $table_name Name of table. Defaults to $this->table.
+     * @return stdClass|null
      */
-    protected function first(array $data): ?\stdClass
+    protected function first(array $data, string $table_name = ""): stdClass|null
     {
-        $result = $this->where($data);
+        $result = $this->where($data, $table_name);
 
         if (!$result) {
             return null;
@@ -66,12 +74,14 @@ trait Model
     /**
      * Insert a record in the table
      * @param array $data An associative array representing the values to be inserted.
+     * @param string $table_name Name of table. Defaults to $this->table.
      * @return void
      */
-    protected function insert(array $data): void
+    protected function insert(array $data, string $table_name = ""): void
     {
+        $table_name = empty($table_name) ? $this->table : $table_name;
         $keys = array_keys($data);
-        $query = "INSERT INTO $this->table(" . join(", ", $keys) . ") ";
+        $query = "INSERT INTO $table_name(" . join(", ", $keys) . ") ";
         $query .= "VALUES (";
 
         // add placeholders to query
@@ -92,14 +102,16 @@ trait Model
      *
      * @param int|string $id The value of the primary key (ID) identifying the record to be updated.
      * @param array $data An associative array representing the columns and their new values to be updated.
+     * @param string $table_name Name of table. Defaults to $this->table.
      * @param string $id_column The name of the ID column. Default is 'id'.
      *
      * @return void
      */
-    protected function update(int|string $id, array $data, string $id_column = 'id'): void
+    protected function update(int|string $id, array $data, string $table_name, string $id_column = 'id'): void
     {
+        $table_name = empty($table_name) ? $this->table : $table_name;
         $keys = array_keys($data);
-        $query = "UPDATE $this->table SET ";
+        $query = "UPDATE $table_name SET ";
 
         // add placeholders to query
         foreach ($keys as $key) {
@@ -118,12 +130,15 @@ trait Model
     /**
      * Delete a record from the table
      * @param mixed $id value of column name in WHERE clause.
+     * @param string $table_name Name of table. Defaults to $this->table.
      * @param string $id_column primary key of table or name of column in WHERE clause.
      * @return void
      */
-    protected function delete(mixed $id, string $id_column = 'id'): void
+    protected function delete(mixed $id, string $table_name, string $id_column = 'id'): void
     {
-        $query = "DELETE FROM $this->table WHERE $id_column = :id";
+        $table_name = empty($table_name) ? $this->table : $table_name;
+
+        $query = "DELETE FROM $table_name WHERE $id_column = :id";
         self::query($query, array($id_column => $id));
     }
 }
