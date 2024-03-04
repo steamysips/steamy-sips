cf8b3df9968fa86e06070b9408fc04488b9164e1
creme332 <65414576+creme332@users.noreply.github.com>
2024-03-04 14:46:30 +0400

    complete getByID, save, validate methods

--- src/models/Client.php
+++ src/models/Client.php
@@ -14,17 +14,93 @@ class Client extends User
 
     private string $city;
 
-    public static function getByID(int $id)
+    public static function getByID(int $id): Client|false
     {
         $query = <<<EOL
-        SELECT * FROM user, administrator
-        WHERE user.user_id = administrator.user_id
+        SELECT * FROM user
+        INNER JOIN client
+        ON user.user_id = client.user_id
+        WHERE user.user_id = :user_id;
         EOL;
 
-        $query .= " AND user_id = $id;";
-
-//        $result = self::query($query);
         Utility::show($query);
+
+        $result = self::query($query, array('user_id' => $id));
+        Utility::show($result);
+
+        if (!$result) {
+            return false;
+        }
+
+        $result = $result[0];
+
+        return new Client(
+            $result->email,
+            $result->first_name,
+            $result->last_name,
+            $result->password,
+            $result->phone_no,
+            new District($result->district_id),
+            $result->street,
+            $result->city
+        );
+    }
+
+    public function save(): void
+    {
+        // if attributes of object are invalid, exit
+        if (count($this->validate()) > 0) {
+            Utility::show($this->validate());
+            return;
+        }
+
+        // get data to be inserted to user table
+        $user_data = $this->toArray();
+        unset($user_data['user_id']);
+        unset($user_data['district']);
+        unset($user_data['street']);
+        unset($user_data['city']);
+
+        // perform insertion to user table
+        $this->insert($user_data, 'user');
+
+        $inserted_record = self::first($user_data, 'user');
+
+        if (!$inserted_record) {
+            return;
+        }
+
+        // get data to be inserted to client table
+        $client_data = (array)[
+            'user_id' => $inserted_record->user_id,
+            'street' => $this->street,
+            'city' => $this->city,
+            'district_id' => $this->district->getID()
+        ];
+
+        // perform insertion to client table
+        $this->insert($client_data, $this->table);
+    }
+
+
+    public function validate(): array
+    {
+        $errors = parent::validate(); // list of errors
+
+        // perform existence checks
+        if (empty($this->district->getName())) {
+            $errors['district'] = 'District name is required';
+        }
+
+        if (empty($this->city)) {
+            $errors['city'] = 'City name is required';
+        }
+
+        if (empty($this->street)) {
+            $errors['street'] = 'Street name is required';
+        }
+
+        return $errors;
     }
 
     public function __construct(
