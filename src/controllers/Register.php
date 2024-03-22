1b6f85eaa7174c33aa0c5c3c4bbfad032b80bf60
creme332 <65414576+creme332@users.noreply.github.com>
2024-03-22 16:45:07 +0400

    update registration form based on new database schema

--- src/controllers/Register.php
+++ src/controllers/Register.php
@@ -4,7 +4,8 @@ namespace Steamy\Controller;
 
 use Steamy\Core\Controller;
 use Steamy\Core\Utility;
-use Steamy\Model\User;
+use Steamy\Model\Client;
+use Steamy\Model\District;
 
 class Register
 {
@@ -12,30 +13,60 @@ class Register
 
     public function index(): void
     {
-        $user = new User();
-        $data['defaultName'] = ''; // initial value for name before submit
-        $data['defaultPassword'] = ''; // initial value for password before submit
-        $data['defaultConfirmPassword'] = ''; // initial  value for confirm password
+        $data['defaultFirstName'] = "";
+        $data['defaultLastName'] = "";
+        $data['defaultPhoneNumber'] = "";
+        $data['defaultDistrictID'] = 7;
+        $data['defaultStreet'] = "";
+        $data['defaultCity'] = "";
+        $data['defaultEmail'] = "";
+        $data['defaultPassword'] = "";
+        $data['defaultConfirmPassword'] = "";
 
         if (isset($_POST['register_submit'])) {
-            $data['defaultName'] = $_POST['name'];
-            $data['defaultPassword'] = $_POST['password'];
-            $data['defaultConfirmPassword'] = $_POST['confirmPassword'];
+            // sanitize data
+            // update default form values
+            $data['defaultFirstName'] = $_POST['first_name'] ?? "";
+            $data['defaultLastName'] = $_POST['last_name'] ?? "";
+            $data['defaultPhoneNumber'] = $_POST['phone_no'] ?? "";
+            $data['defaultDistrictID'] = $_POST['district'] ?? 1;
+            $data['defaultStreet'] = $_POST['street'] ?? "";
+            $data['defaultCity'] = $_POST['city'] ?? "";
+            $data['defaultEmail'] = $_POST['email'] ?? "";
+            $data['defaultPassword'] = $_POST['password'] ?? "";
+            $data['defaultConfirmPassword'] = $_POST['confirmPassword'] ?? "";
 
-            if ($user->validate($_POST)) {
-                // remove extraneous $post values before insertion to database
-                unset($_POST['register_submit']);
-                unset($_POST['confirmPassword']);
-                $user->insert($_POST);
+            // create a new client object
+            $client = new Client(
+                $data['defaultEmail'],
+                $data['defaultFirstName'],
+                $data['defaultLastName'],
+                $data['defaultPassword'],
+                $data['defaultPhoneNumber'],
+                new District($data['defaultDistrictID']),
+                $data['defaultStreet'],
+                $data['defaultCity']
+            );
+
+            // validate all attributes
+            $data['errors'] = $client->validate();
+
+            // check if passwords do not match
+            if ($data['defaultConfirmPassword'] !== $data['defaultPassword']) {
+                $data['errors']['confirmPassword'] = 'Passwords do not match';
+            }
+
+            // if all data valid, save new record and redirect to login page
+            if (empty($data['errors'])) {
+                $client->save();
                 Utility::redirect('login');
             }
-            $data['errors'] = $user->errors;
         }
 
         $this->view(
             'Register',
             $data,
-            'Login'
+            'Register'
         );
     }
 }
--- src/views/Register.php
+++ src/views/Register.php
@@ -1,9 +1,15 @@
 <?php
 /**
- * Variables below are defined in controller.
- * @var string $defaultName Default name in registration form
- * @var string $defaultPassword Default password in registration form
- * @var string $defaultConfirmPassword Default password in registration form
+ * Variables below are defined and initialized in Register controller.
+ * @var string $defaultFirstName
+ * @var string $defaultLastName
+ * @var string $defaultPhoneNumber
+ * @var string $defaultStreet
+ * @var string $defaultCity
+ * @var string $defaultDistrictID
+ * @var string $defaultEmail
+ * @var string $defaultPassword
+ * @var string $defaultConfirmPassword
  */
 
 ?>
@@ -15,44 +21,115 @@
                 <h2>Create a new account for free</h2>
             </hgroup>
             <form method="post">
-                <label for="name">Name</label>
-                <input id="name" type="text" name="name" aria-label="Name" autocomplete="nickname"
-                       value="<?php
-                       echo $defaultName; ?>" required/>
-                <?php
-                if (!empty($errors['name'])) : ?>
-                    <small class="warning"><?php
-                        echo $errors['name'] ?></small>
-                <?php
-                endif; ?>
+
+                <fieldset>
+                    <legend><strong>Personal details</strong></legend>
+                    <div class="grid">
+
+                        <div class="container">
+                            <label for="first_name">First Name</label>
+                            <input autofocus id="first_name" type="text" name="first_name"
+                                   value="<?= $defaultFirstName ?>" required/>
+                            <?php
+                            if (!empty($errors['name'])) : ?>
+                                <small class="warning"><?php
+                                    echo $errors['name'] ?></small>
+                            <?php
+                            endif;
+                            ?>
+                        </div>
 
 
-                <div class="grid">
-                    <div class="container">
-                        <label for="password">Password</label>
-                        <input id="password" type="password" name="password" aria-label="Password"
-                               value="<?php
-                               echo $defaultPassword; ?>" autocomplete="current-password" required/>
-                        <?php
-                        if (!empty($errors['password'])) : ?>
-                            <small class="warning"><?php
-                                echo $errors['password'] ?></small>
-                        <?php
-                        endif; ?>
+                        <div class="container">
+                            <label for="last_name">Last Name</label>
+                            <input id="last_name" type="text" name="last_name"
+                                   value="<?= $defaultLastName ?>" required/>
+                            <?php
+                            if (!empty($errors['name'])) : ?>
+                                <small class="warning"><?php
+                                    echo $errors['name'] ?></small>
+                            <?php
+                            endif;
+                            ?>
+                        </div>
+
                     </div>
-                    <div class="container">
-                        <label for="confirmPassword">Confirm password</label>
-                        <input id="confirmPassword" type="password" name="confirmPassword" aria-label="Confirm password"
-                               value="<?php
-                               echo $defaultConfirmPassword; ?>" required/>
-                        <?php
-                        if (!empty($errors['confirmPassword'])) : ?>
-                            <small class="warning"><?php
-                                echo $errors['confirmPassword'] ?></small>
-                        <?php
-                        endif; ?>
+
+                    <label class="container" for="telephone">
+                        Phone Number
+                        <small>(eg., +230-5-123-4567)</small>
+                    </label>
+                    <input value="<?= $defaultPhoneNumber ?>" id="telephone" type="tel" name="phone_no"
+                           pattern="\+230-5-[0-9]{3}-[0-9]{4}"
+                           title="eg., +230-5-123-4567" required/>
+
+                    <div class="grid">
+                        <div class="container">
+                            <label for="street">Street</label>
+                            <input name="street" value="<?= $defaultStreet ?>" id="street" type="text">
+                        </div>
+
+                        <div class="container">
+                            <label for="city">City</label>
+                            <input name="city" value="<?= $defaultCity ?>" id="city" type="text">
+                        </div>
                     </div>
-                </div>
+
+                    <label for="districts">District</label>
+                    <!--  TODO: generate district list dynamically-->
+                    <select name="district" id="districts">
+                        <option value="8" <?= $defaultDistrictID == 8 ? "selected" : "" ?>>Pamplemousses</option>
+                        <option value="7" <?= $defaultDistrictID == 7 ? "selected" : "" ?>>Riviere du Rempart</option>
+                        <option value="2" <?= $defaultDistrictID == 2 ? "selected" : "" ?>>Port Louis</option>
+                        <option value="1" <?= $defaultDistrictID == 1 ? "selected" : "" ?>>Moka</option>
+                        <option value="3" <?= $defaultDistrictID == 3 ? "selected" : "" ?>>Flacq</option>
+                        <option value="9" <?= $defaultDistrictID == 9 ? "selected" : "" ?>>Plaines Wilhems</option>
+                        <option value="4" <?= $defaultDistrictID == 4 ? "selected" : "" ?>>Black River</option>
+                        <option value="5" <?= $defaultDistrictID == 5 ? "selected" : "" ?>>Savanne</option>
+                        <option value="6" <?= $defaultDistrictID == 6 ? "selected" : "" ?>>Grand Port</option>
+                    </select>
+
+
+                </fieldset>
+
+                <fieldset>
+                    <legend><strong>Account information</strong></legend>
+                    <label for="email">Email</label>
+                    <input value="<?= $defaultEmail ?>" id="email" type="email" name="email" required/>
+
+                    <div class="grid">
+                        <div class="container">
+                            <label for="password">Password</label>
+                            <input id="password" type="password" name="password"
+                                   aria-label="Password"
+                                   value="<?= $defaultPassword ?>"
+                                   required/>
+
+                            <?php
+                            if (!empty($errors['password'])) : ?>
+                                <small class="warning"><?= $errors['password'] ?></small>
+                            <?php
+                            endif;
+                            ?>
+                        </div>
+
+                        <div class="container">
+                            <label for="confirmPassword">Confirm password</label>
+                            <input id="confirmPassword" type="password" name="confirmPassword"
+                                   aria-label="Confirm password"
+                                   value="<?= $defaultConfirmPassword ?>" required/>
+                            <?php
+                            if (!empty($errors['confirmPassword'])) : ?>
+                                <small class="warning"><?= $errors['confirmPassword'] ?></small>
+                            <?php
+                            endif; ?>
+                        </div>
+                    </div>
+                    <label>
+                        <small>Show password</small>
+                        <input type="checkbox" onclick="togglePasswordVisibility()">
+                    </label>
+                </fieldset>
 
                 <button type="submit" name="register_submit">Register</button>
                 <small>Already have an account? <a href="<?= ROOT ?>/login">Login</a></small>
@@ -60,3 +137,20 @@
         </div>
     </article>
 </main>
+
+<script>
+  function togglePasswordVisibility() {
+    // Reference: https://www.w3schools.com/howto/howto_js_toggle_password.asp
+    const passwordInput = document.getElementById("password");
+    const confirmPasswordInput = document.getElementById("confirmPassword");
+
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      confirmPasswordInput.type = "text";
+    } else {
+      passwordInput.type = "password";
+      confirmPasswordInput.type = "password";
+
+    }
+  }
+</script>
