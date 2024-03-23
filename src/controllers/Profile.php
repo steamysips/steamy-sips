b105fff21cb3cf8885647a68c472d60316d6bb70
creme332 <65414576+creme332@users.noreply.github.com>
2024-03-23 12:48:40 +0400

    refactor and implement account deletion

--- src/controllers/Profile.php
+++ src/controllers/Profile.php
@@ -4,31 +4,71 @@ namespace Steamy\Controller;
 
 use Steamy\Core\Controller;
 use Steamy\Core\Utility;
-use Steamy\Model\User;
+use Steamy\Model\Client;
 
 class Profile
 {
     use Controller;
 
-    public function index(): void
+    private Client $client;
+    private array $data;
+
+    public function __construct()
     {
-        // if user is not signed in, redirect to login page
+        // filter out unsigned users
+        $this->handleUnsignedUsers();
+
+        // fetch user details from database
+        $this->client = Client::getByEmail($_SESSION['user']);
+    }
+
+    private function handleLogOut(): void
+    {
+        $_SESSION = array();
+        session_destroy();
+
+        // regenerate session id for security purposes
+        // Reference: https://stackoverflow.com/a/34206189/17627866
         session_regenerate_id();
+
+        // redirect user to login page
+        Utility::redirect('login');
+    }
+
+    private function handleAccountDeletion(): void
+    {
+        // delete user account if delete button clicked
+        $this->client->deleteUser();
+        $this->handleLogOut();
+    }
+
+    /**
+     * Check if current user is signed in and if not redirects
+     * him to login page
+     *
+     * @return void
+     */
+    private function handleUnsignedUsers(): void
+    {
         if (!array_key_exists('user', $_SESSION) || !isset($_SESSION['user'])) {
             Utility::redirect('login');
         }
+    }
 
+    public function index(): void
+    {
         // log out user if logout button clicked
         if (isset($_POST['logout_submit'])) {
-            $_SESSION = array();
-            Utility::redirect('login');
+            $this->handleLogOut();
         }
 
-        // fetch user details from database
-        $current_user = new User();
+        // delete user account if delete button clicked
+        if (isset($_POST['account_delete_submit'])) {
+            $this->handleAccountDeletion();
+        }
 
-        // fetch 5 latest orders
-        $data["orders"] = array_fill(
+        // TODO: fetch 5 latest orders
+        $this->data["orders"] = array_fill(
             0,
             5,
             (object)[
@@ -39,10 +79,14 @@ class Profile
             ]
         );
 
-        // display user profile
+        // initialize user details for template
+        $this->data["name"] = $this->client->getFullName();
+        $this->data["email"] = $this->client->getEmail();
+        $this->data["address"] = $this->client->getAddress();
+
         $this->view(
             'Profile',
-            $data,
+            $this->data,
             'Profile'
         );
     }
