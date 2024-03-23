de2e3ca9859d25b827fb485356fe6be14969b369
creme332 <65414576+creme332@users.noreply.github.com>
2024-03-23 11:03:48 +0400

    update meaning of password in constructor, add new methods concerning passwords

--- src/models/User.php
+++ src/models/User.php
@@ -13,20 +13,27 @@ abstract class User
     protected string $email = "";
     protected string $first_name = "";
     protected string $last_name = "";
-    protected string $password = "";
+    protected string $password = ""; // hash of original password
     protected string $phone_no = "";
 
+    /**
+     * @param string $email
+     * @param string $first_name
+     * @param string $last_name
+     * @param string $plain_password plain (original) version of password
+     * @param string $phone_no
+     */
     public function __construct(
         string $email,
         string $first_name,
         string $last_name,
-        string $password,
+        string $plain_password,
         string $phone_no
     ) {
         $this->email = $email;
         $this->first_name = $first_name;
         $this->last_name = $last_name;
-        $this->password = password_hash($password, PASSWORD_BCRYPT);
+        $this->password = password_hash($plain_password, PASSWORD_BCRYPT);
         $this->phone_no = $phone_no;
     }
 
@@ -43,6 +50,35 @@ abstract class User
             ];
     }
 
+    /**
+     * Validate plain password.
+     *
+     * @param string $plain_password The plain text password to validate.
+     * @return string[] List of all errors.
+     */
+    public static function validatePlainPassword(string $plain_password): array
+    {
+        $errors = []; // List of errors
+
+        // Check if password is empty
+        if (empty($plain_password)) {
+            $errors[] = "Password is required";
+        }
+
+        // Check if password length is within acceptable limits
+        $min_length = 1;
+        $max_length = 32;
+        $password_length = strlen($plain_password);
+        if ($password_length < $min_length || $password_length > $max_length) {
+            $errors[] = "Password must be between $min_length and $max_length characters long";
+        }
+
+        // TODO: Add more validation checks here, such as checking for specific characters,
+        // enforcing complexity requirements, etc.
+
+        return $errors;
+    }
+
     public function validate(): array
     {
         $errors = []; // list of errors
@@ -54,9 +90,6 @@ abstract class User
 
         if (empty($this->first_name)) {
             $errors['first_name'] = "First name is required";
-        }
-        if (empty($this->password)) {
-            $errors['password'] = "Password is required";
         }
 
         if (empty($this->phone_no)) {
@@ -86,9 +119,24 @@ abstract class User
         $this->last_name = $last_name;
     }
 
-    public function setPassword(string $original_password): void
+    /**
+     * Changes the hash of the user password
+     * @param string $new_password_hash new password hash
+     * @return void
+     */
+    public function setPassword(string $new_password_hash): void
     {
-        $this->password = password_hash($original_password, PASSWORD_BCRYPT);
+        $this->password = $new_password_hash;
+    }
+
+    /**
+     * Checks if the password hash of the user matches parameter
+     * @param string $plain_password
+     * @return bool
+     */
+    public function verifyPassword(string $plain_password): bool
+    {
+        return password_verify($this->password, $plain_password);
     }
 
     public function getEmail(): string
@@ -119,10 +167,5 @@ abstract class User
     public function getUserID(): int
     {
         return $this->user_id;
-    }
-
-    public function getPassword(): string
-    {
-        return $this->password;
     }
 }
