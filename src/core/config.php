a5efb0cfe38366570dafe2de2100c1b4b75c4b44
creme332 <65414576+creme332@users.noreply.github.com>
2024-03-25 11:52:50 +0400

    update environment variables for testing database

--- src/core/config.php
+++ src/core/config.php
@@ -4,21 +4,19 @@
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-if ($_ENV['APP_ENV'] == 'dev') {
-    // setup configurations localhost server
-    define('DB_HOST', 'localhost');
-
-    // define absolute url for public folder
-    define('ROOT', $_ENV['DEV_ROOT']);
-}
-
-if ($_ENV['APP_ENV'] == 'prod') {
-    // setup configurations for production server
-    define('DB_HOST', $_ENV['PROD_DB_HOST']);
-    define('ROOT', $_ENV['PROD_ROOT']);
-}
+// define absolute URL to public folder
+define('ROOT', $_ENV['PUBLIC_ROOT']);
 
 // define database credentials
-define('DB_NAME', $_ENV['DB_NAME']);
+define('DB_HOST', $_ENV['DB_HOST']);
 define('DB_USERNAME', $_ENV['DB_USERNAME']);
 define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+
+if (defined('PHPUNIT_STEAMY_TESTSUITE') && PHPUNIT_STEAMY_TESTSUITE) {
+    // application is currently being tested with phpunit => use testing database
+    define('DB_NAME', $_ENV['TEST_DB_NAME']);
+} else {
+    // application is running normally => use production database
+    define('DB_NAME', $_ENV['PROD_DB_NAME']);
+}
+
