cfba0b8dde95cff92bfae66aecba8a88532ae456
creme332 <65414576+creme332@users.noreply.github.com>
2024-03-29 13:15:11 +0400

    add handleReviewSubmission(), add attribute signed_user

--- src/controllers/Product.php
+++ src/controllers/Product.php
@@ -6,6 +6,9 @@ namespace Steamy\Controller;
 
 use Steamy\Core\Controller;
 use Steamy\Core\Utility;
+use Steamy\Model\Client;
+use Steamy\Model\Review;
+use Steamy\Model\User;
 
 /**
  * Displays product page when URL follows format `/shop/products/<number>`.
@@ -14,78 +17,101 @@ class Product
 {
     use Controller;
 
-    private \Steamy\Model\Product $product;
-    private bool $invalidProductID = false;
+    private ?\Steamy\Model\Product $product = null; // product to be displayed
+    private array $view_data;
+    private ?User $signed_user = null; // currently logged-in user
 
     public function __construct()
     {
+        // initialize some view data
+        $this->view_data["default_review"] = "";
+        $this->view_data["default_rating"] = "";
+        $this->view_data["signed_in_user"] = null;
+        $this->view_data["product"] = null;
+
         // get product id from URL
         $product_id = filter_var(Utility::splitURL()[2], FILTER_VALIDATE_INT);
 
-        if (!$product_id) {
-            $this->invalidProductID = true;
+        // check if user is logged in
+        $reviewer_email = $_SESSION['user'] ?? "";
+
+        // get user details
+        $user_account = Client::getByEmail($reviewer_email);
+        if (!empty($user_account)) {
+            $this->signed_user = $user_account;
+            $this->view_data["signed_in_user"] = $user_account;
+        }
+
+        // if product id valid fetch product from db
+        if ($product_id) {
+            $fetched_product = \Steamy\Model\Product::getByID($product_id);
+
+            if ($fetched_product) {
+                $this->product = $fetched_product;
+                $this->view_data["product"] = $this->product;
+            }
+        }
+    }
+
+    private function handleReviewSubmission(): void
+    {
+        $new_comment = trim($_POST['review_text'] ?? "");
+        $rating = filter_var($_POST['review_rating'], FILTER_VALIDATE_INT);
+
+        // unset POST values for next submission. this prevents duplicate form submission
+//        unset($_POST['review_text']);
+//        unset($_POST['review_rating']);
+
+        // ignore requests from users who are not logged in
+        if (empty($this->signed_user)) {
             return;
         }
 
-        // fetch product from database
-        $fetched_product = \Steamy\Model\Product::getByID($product_id);
+        $review = new Review(
+            $this->signed_user->getUserID(),
+            $this->product->getProductID(),
+            null,
+            $new_comment,
+            $rating,
+            new \DateTime()
+        );
 
-        if (!$fetched_product) {
-            $this->invalidProductID = true;
+        $this->view_data['errors'] = $review->validate();
+
+        // check if review attributes are valid
+        if (empty($this->view_data['errors'])) {
+            // save to database
+            $review->save();
         } else {
-            $this->product = $fetched_product;
+            // form values are invalid
+            Utility::show($this->view_data['errors']);
+
+            // set default values on form for submitting reviews
+            $this->view_data["default_review"] = $new_comment;
+            $this->view_data["default_rating"] = $rating;
         }
     }
 
     public function index(): void
     {
-        if ($this->invalidProductID) {
-            // if product was not found, display error page
+        // if product was not found, display error page
+        if (empty($this->product)) {
             $this->view(
                 '404',
                 template_title: 'Product not found'
             );
             return;
         }
-        $data["product"] = $this->product;
 
-        // get all reviews for product
-        $data["reviews"] = array_fill(
-            0,
-            10,
-            (object)[
-                'date' => 'April 9, 2023',
-                'author' => 'User123',
-                'text' => 'This is a comment.',
-                'children' => [
-                    (object)[
-                        'date' => 'April 10, 2023',
-                        'author' => 'Administrator',
-                        'text' => 'This is a comment.',
-                        'children' => [
-                            (object)[
-                                'date' => 'April 9, 2023',
-                                'author' => 'User123',
-                                'text' => 'Yes.',
-                                'children' => [
-                                    (object)[
-                                        'date' => 'April 9, 2023',
-                                        'author' => 'User123',
-                                        'text' => 'This is a comment.',
-                                        'children' => []
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-        );
+        // handle review submission
+        if (isset($_POST['review_text']) || isset($_POST['review_rating'])) {
+            $this->handleReviewSubmission();
+        }
 
         $this->view(
             'Product',
-            $data,
-            $data["product"]->getName() . ' | Steamy Sips'
+            $this->view_data,
+            $this->product->getName() . ' | Steamy Sips'
         );
     }
 }
