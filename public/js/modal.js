9b0b641d5848a9da11028eb3fbb07e634dd48abe
creme332 <65414576+creme332@users.noreply.github.com>
2024-04-10 11:14:53 +0400

    create a general function for modal management

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,113 +1,119 @@
-/*
- * Modal
+/**
+ * A function for managing a modal
  *
- * Reference: https://github.com/picocss/examples/blob/master/v1-preview/js/modal.js
- * Pico.css - https://picocss.com
- * Copyright 2019-2023 - Licensed under MIT
+ * @param {string} modalID ID of modal in document
+ * @returns {{ init: (function(): void), openModal: (function(): void), closeModal: (function(): void)}}
+ * @constructor
+ *
+ *  Adapted from: https://github.com/picocss/examples/blob/master/v1-preview/js/modal.js
+ *  Pico.css - https://picocss.com
+ *  Copyright 2019-2023 - Licensed under MIT
  */
+function ModalManager(modalID) {
+  const isOpenClass = "modal-is-open";
+  const openingClass = "modal-is-opening";
+  const closingClass = "modal-is-closing";
+  const animationDuration = 600; // ms
+  let visibleModal = null;
+  const modal = document.getElementById(modalID);
 
-// Config
-const isOpenClass = "modal-is-open";
-const openingClass = "modal-is-opening";
-const closingClass = "modal-is-closing";
-const animationDuration = 600; // ms
-let visibleModal = null;
+  // Toggle modal
+  const toggleModal = (event) => {
+    event.preventDefault();
 
-// Toggle modal
-const toggleModal = (event) => {
-  event.preventDefault();
-  const modal = document.getElementById(
-    event.currentTarget.getAttribute("data-target"),
-  );
-  typeof modal != "undefined" && modal != null && isModalOpen(modal)
-    ? closeModal(modal)
-    : openModal(modal);
-};
+    typeof modal != "undefined" && modal != null && isModalOpen(modal)
+      ? closeModal(modal)
+      : openModal(modal);
+  };
 
-// Is modal open
-const isModalOpen = (modal) => {
-  return modal.hasAttribute("open") && modal.getAttribute("open") !== "false";
-};
+  // Is modal open
+  const isModalOpen = () => {
+    return modal.hasAttribute("open") && modal.getAttribute("open") !== "false";
+  };
 
-// Open modal
-const openModal = (modal) => {
-  if (isScrollbarVisible()) {
-    document.documentElement.style.setProperty(
-      "--scrollbar-width",
-      `${getScrollbarWidth()}px`,
-    );
-  }
-  document.documentElement.classList.add(isOpenClass, openingClass);
-  setTimeout(() => {
-    visibleModal = modal;
-    document.documentElement.classList.remove(openingClass);
-  }, animationDuration);
-  modal.setAttribute("open", true);
-};
+  // Open modal
+  const openModal = () => {
+    if (isScrollbarVisible()) {
+      document.documentElement.style.setProperty(
+        "--scrollbar-width",
+        `${getScrollbarWidth()}px`,
+      );
+    }
+    document.documentElement.classList.add(isOpenClass, openingClass);
+    setTimeout(() => {
+      visibleModal = modal;
+      document.documentElement.classList.remove(openingClass);
+    }, animationDuration);
+    modal.setAttribute("open", true);
+  };
 
-// Close modal
-const closeModal = (modal) => {
-  visibleModal = null;
-  document.documentElement.classList.add(closingClass);
-  setTimeout(() => {
-    document.documentElement.classList.remove(closingClass, isOpenClass);
-    document.documentElement.style.removeProperty("--scrollbar-width");
-    modal.removeAttribute("open");
-  }, animationDuration);
-};
+  // Close modal
+  const closeModal = () => {
+    visibleModal = null;
+    document.documentElement.classList.add(closingClass);
+    setTimeout(() => {
+      document.documentElement.classList.remove(closingClass, isOpenClass);
+      document.documentElement.style.removeProperty("--scrollbar-width");
+      modal.removeAttribute("open");
+    }, animationDuration);
+  };
 
-// Close with a click outside
-document.addEventListener("click", (event) => {
-  if (visibleModal != null) {
-    const modalContent = visibleModal.querySelector("article");
-    const isClickInside = modalContent.contains(event.target);
-    !isClickInside && closeModal(visibleModal);
-  }
-});
+  // Get scrollbar width
+  const getScrollbarWidth = () => {
+    // Creating invisible container
+    const outer = document.createElement("div");
+    outer.style.visibility = "hidden";
+    outer.style.overflow = "scroll"; // forcing scrollbar to appear
+    outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
+    document.body.appendChild(outer);
 
-// Close with Esc key
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Escape" && visibleModal != null) {
-    closeModal(visibleModal);
-  }
-});
+    // Creating inner element and placing it in the container
+    const inner = document.createElement("div");
+    outer.appendChild(inner);
 
-// Get scrollbar width
-const getScrollbarWidth = () => {
-  // Creating invisible container
-  const outer = document.createElement("div");
-  outer.style.visibility = "hidden";
-  outer.style.overflow = "scroll"; // forcing scrollbar to appear
-  outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
-  document.body.appendChild(outer);
+    // Calculating difference between container's full width and the child width
+    const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
 
-  // Creating inner element and placing it in the container
-  const inner = document.createElement("div");
-  outer.appendChild(inner);
+    // Removing temporary elements from the DOM
+    outer.parentNode.removeChild(outer);
 
-  // Calculating difference between container's full width and the child width
-  const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+    return scrollbarWidth;
+  };
 
-  // Removing temporary elements from the DOM
-  outer.parentNode.removeChild(outer);
+  // Is scrollbar visible
+  const isScrollbarVisible = () => {
+    return document.body.scrollHeight > screen.height;
+  };
 
-  return scrollbarWidth;
-};
+  const init = () => {
+    // Close modal with a click outside
+    document.addEventListener("click", (event) => {
+      if (visibleModal != null) {
+        const modalContent = visibleModal.querySelector("article");
+        const isClickInside = modalContent.contains(event.target);
+        !isClickInside && closeModal(visibleModal);
+      }
+    });
 
-// Is scrollbar visible
-const isScrollbarVisible = () => {
-  return document.body.scrollHeight > screen.height;
-};
+    // Close modal with Esc key
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Escape" && visibleModal != null) {
+        closeModal(visibleModal);
+      }
+    });
 
-// Add event listeners to modal components
-document
-  .querySelector('#cart-modal article > a[data-target="cart-modal"]')
-  .addEventListener("click", (e) => toggleModal(e));
-document
-  .querySelector('#cart-modal article > footer a[data-target="cart-modal"]')
-  .addEventListener("click", (e) => toggleModal(e));
+    // Add event listeners to modal components
+    document
+      .querySelector(`#${modalID} article > a[data-target="${modalID}"]`)
+      .addEventListener("click", (e) => toggleModal(e)); // close button in top right corner
+    document
+      .querySelector(`#${modalID} article > footer a[data-target="${modalID}"]`)
+      .addEventListener("click", (e) => toggleModal(e)); // close button in footer
 
-// add event listener to shopping cart icon in nav bar
-document
-  .querySelector('nav li[data-target="cart-modal"]')
-  .addEventListener("click", (e) => toggleModal(e));
+    console.log("Event listeners added for modal");
+  };
+
+  return { openModal, closeModal, init };
+}
+
+export default ModalManager;
