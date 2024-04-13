4c90965ca0e7b7c0118409a1783dd605ef3a68bb
creme332 <65414576+creme332@users.noreply.github.com>
2024-04-13 11:34:53 +0400

    refactor login and add output sanitization

--- src/views/Login.php
+++ src/views/Login.php
@@ -2,10 +2,9 @@
 
 declare(strict_types=1);
 /**
- * Variables below are defined in Login controller
- * @var string $defaultEmail
- * @var string $defaultPassword
- * @var array $errors Error if any is available in $errors['other']
+ * View variables defined in Login controller:
+ *
+ * @var string $defaultEmail Email set in form
  */
 ?>
 
@@ -15,12 +14,12 @@ declare(strict_types=1);
             <h1>Sign in</h1>
             <form method="post">
                 <input autofocus type="email" name="email" placeholder="Email" aria-label="Email"
-                       aria-invalid="<?= isset($_POST['login_submit']) && !empty($errors['other']) ? 'true' : '' ?>"
+                       aria-invalid="<?= isset($_POST['login_submit']) ? 'true' : '' ?>"
                        value="<?= $defaultEmail ?>" required/>
 
                 <input type="password" name="password" placeholder="Password" aria-label="Password"
-                       aria-invalid="<?= isset($_POST['login_submit']) && !empty($errors['other']) ? 'true' : '' ?>"
-                       value="<?= $defaultPassword ?>" required/>
+                       aria-invalid="<?= isset($_POST['login_submit']) ? 'true' : '' ?>"
+                       required/>
 
                 <button name="login_submit" type="submit" class="contrast">Login</button>
                 <small>Don't have an account yet? <a href="<?= ROOT ?>/register">Register</a></small>
