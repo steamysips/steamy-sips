50545decbf453fbb237c39c8027673fded38c2f7
creme332 <65414576+creme332@users.noreply.github.com>
2024-04-20 21:51:02 +0400

    refactor and fix bugs.
    remaining bugs come from User static methods

--- src/controllers/Password.php
+++ src/controllers/Password.php
@@ -1,86 +1,59 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Steamy\Controller;
 
 use Steamy\Core\Utility;
 use Steamy\Model\User;
 use Steamy\Core\Controller;
 
-// Define PUBLIC_ROOT constant if not defined
-if (!defined('PUBLIC_ROOT')) {
-    define('PUBLIC_ROOT', 'http://localhost:8080/steamy-sips/public');
-}
-
+/**
+ * Displays form asking for email and handles email submission for password reset.
+ */
 class Password
 {
-    use Controller; 
+    use Controller;
 
-    public function index()
+    private function handleEmailSubmission(): void
     {
-        // If email submitted
-        if (isset($_POST['usersEmail']) && filter_var($_POST['usersEmail'], FILTER_VALIDATE_EMAIL)) {
-            $email = $_POST['usersEmail'];
+        $submitted_email = filter_var($_POST['email'] ?? "", FILTER_VALIDATE_EMAIL);
 
-            // Generate random token
-            $token = bin2hex(random_bytes(16)); // Generating a random token of length 32 bytes (hexadecimal format)
-
-            // Save information about request in the password_change_request table
-            $expiryDate = date('Y-m-d H:i:s', strtotime('+1 day')); // Expiry date set to 1 day from now
-            $tokenHash = password_hash($token, PASSWORD_DEFAULT); // Hashing the token for security
-            $userId = User::getUserIdByEmail($email); // Get user ID by email
-
-            if ($userId !== false) {
-                User::savePasswordChangeRequest($userId, $tokenHash, $expiryDate);
-                
-                // Send email with reset link
-                $resetLink = PUBLIC_ROOT . "/password/reset?id=$token";
-                User::sendResetEmail($email, $resetLink);
-
-            } else {
-                Utility::redirect('login');
-            }
-         } else {
-            // Display forgot password form
-            require_once PUBLIC_ROOT . 'resetpassword.php';
+        if (empty($submitted_email)) {
+            return;
         }
 
-        $this->view(
-            'ResetPassword',
-            ['email' => $email], // Pass email data to the view
-            'ResetPassword'
-        );
+        // email is valid
 
-        if (isset($_POST['pwd'], $_POST['pwd-repeat'], $_GET['id'])) {
-            $password = $_POST['pwd'];
-            $passwordRepeat = $_POST['pwd-repeat'];
-            $token = $_GET['id'];
+        // Generate random token
+        $token = bin2hex(random_bytes(16)); // Generating a random token of length 32 bytes (hexadecimal format)
 
-            // Check if passwords match
-            if ($password === $passwordRepeat) {
-                // Hash the new password
-                $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        // Save information about request in the password_change_request table
+        $expiryDate = date('Y-m-d H:i:s', strtotime('+1 day')); // Expiry date set to 1 day from now
+        $tokenHash = password_hash($token, PASSWORD_BCRYPT); // Hashing the token for security
+        $userId = User::getUserIdByEmail($submitted_email); // Get user ID by email
 
-                // Get user ID based on token
-                $userId = User::getUserIdByToken($token);
-
-                if ($userId !== false) {
-                    // Update user's password
-                    User::updatePassword($userId, $hashedPassword);
-
-                    // Redirect to login page
-                    Utility::redirect('login');
-                }
-            } else {        
-                // If passwords don't match or token is invalid, redirect to reset password page
-                Utility::redirect('resetpassword');	
-            }
+        if ($userId !== false) {
+            User::savePasswordChangeRequest($userId, $tokenHash, $expiryDate);
+            // Send email with reset link
+            $resetLink = ROOT . "/password/reset?id=$token";
+            User::sendResetEmail($submitted_email, $resetLink);
+        } else {
+            Utility::redirect('login');
         }
+    }
 
-        $this->view(
-            'Newpassword',
-            ['pwd' => '', 'pwd-repeat' => ''], 
-            'Newpassword'
-        );
-
+    public function index(): void
+    {
+        if (isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $this->handleEmailSubmission();
+        }
+        // display form asking for user email
+        {
+            $this->view(
+                view_name: 'ResetPassword',
+                template_title: 'Reset Password'
+            );
+        }
     }
 }
--- src/views/ResetPassword.php
+++ src/views/ResetPassword.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 ?>
 
-<h1 class="header">Reset Password</h1>
+<h1>Reset Password</h1>
 
-    <form method="post" action="">
-        <input type="hidden" name="type" value="send" />
-        <input type="text" name="usersEmail" placeholder="Email...">
-        <button type="submit" name="submit">Receive Email</button>
-    </form>
+<form method="post" action="">
+    <input type="text" name="email" placeholder="Email...">
+    <button type="submit">Receive Email</button>
+</form>
