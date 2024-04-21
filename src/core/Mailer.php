4544135948ccd90e43fc36a6ea5c42a66a373186
divyesh000 <divjok28@outlook.com>
2024-04-21 18:24:52 +0400

    SMTP::DEBUG_SERVER to SMTP::DEBUG_OFF

--- src/core/Mailer.php
+++ src/core/Mailer.php
@@ -33,7 +33,7 @@ class Mailer
         //SMTP::DEBUG_OFF = off (for production use)
         //SMTP::DEBUG_CLIENT = client messages
         //SMTP::DEBUG_SERVER = client and server messages
-        $this->mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $this->mail->SMTPDebug = SMTP::DEBUG_OFF;
 
         //Set the hostname of the mail server
         $this->mail->Host = 'smtp.gmail.com';
