b38c2f9fea2e28aa747a9b69aa7ec860dcd2da56
creme332 <65414576+creme332@users.noreply.github.com>
2024-04-25 11:11:45 +0400

    change favicon to coffee

--- src/views/Template.php
+++ src/views/Template.php
@@ -22,7 +22,7 @@ declare(strict_types=1);
     <meta name="apple-mobile-web-app-capable" content="yes"/>
     <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
     <link rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐞</text></svg>"/>
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>☕</text></svg>"/>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css"/>
     <link rel="stylesheet" href="<?= ROOT ?>/styles/global.css"/>
