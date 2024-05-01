5e48b59c5b6da117ecd1022362eb39b4639cb552
creme332 <65414576+creme332@users.noreply.github.com>
2024-05-01 13:39:48 +0400

    use require_once instead of require to load init

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require '../src/core/init.php'; // initialize app
+require_once '../src/core/init.php'; // initialize app
 
 $app = new Steamy\Core\App();
 $app->loadController();
