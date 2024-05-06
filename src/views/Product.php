c7e3c862daca5cf6e87b49cf20eaa18eff6b0f39
creme332 <65414576+creme332@users.noreply.github.com>
2024-05-06 11:49:53 +0400

    display average product rating

--- src/views/Product.php
+++ src/views/Product.php
@@ -346,8 +346,30 @@ endif ?>
         </button>
     </form>
 
-    <div style="width: 500px;">
-        <canvas id="customer_rating_chart" data-chart-data="<?= $rating_distribution ?>"></canvas>
+    <div style="display: flex; align-items: center; gap:3em;">
+        <hgroup>
+            <h6>Average Rating</h6>
+            <p style="font-size: 50px;">
+                <?= filter_var(
+                    $product->getAverageRating(),
+                    FILTER_SANITIZE_NUMBER_FLOAT,
+                    FILTER_FLAG_ALLOW_FRACTION
+                ) ?>
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"
+                     fill="none" stroke="black" stroke-width="2" stroke-linecap="round"
+                     stroke-linejoin="round" class="fill-star icon icon-tabler icons-tabler
+                            -outline icon-tabler-star">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M12 17.75l-6.172 3.245l1.179
+                             -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"/>
+                </svg>
+            </p>
+        </hgroup>
+
+        <div style="width: 500px;">
+            <canvas id="customer_rating_chart" data-chart-data="<?= $rating_distribution ?>"></canvas>
+        </div>
     </div>
 
     <form id="review-form" action="" method="get">
@@ -359,7 +381,8 @@ endif ?>
             </option>
             <option value="verified-reviews"
                 <?= $current_review_filter === 'verified-reviews' ? 'selected' : '' ?>
-            >Verified reviews only</option>
+            >Verified reviews only
+            </option>
         </select>
     </form>
 
