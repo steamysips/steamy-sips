651da890e42507bc9dc606d791f3e42350b56f2e
divyesh000 <divjok28@outlook.com>
2024-05-08 20:45:53 +0400

    change abstract class OrderStatus to enum

--- src/models/OrderStatus.php
+++ src/models/OrderStatus.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Steamy\Model;
 
-abstract class OrderStatus {
-    const PENDING = 'pending';
-    const CANCELLED = 'cancelled';
-    const COMPLETED = 'completed';
+enum OrderStatus {
+    case PENDING;
+    case CANCELLED;
+    case COMPLETED;
 }
