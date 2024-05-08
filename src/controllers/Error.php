d056f0b1cfab40fe09e2d57ee2f1e6fb058c72fc
creme332 <65414576+creme332@users.noreply.github.com>
2024-05-08 12:06:00 +0400

    create a new error controller with reusable error messages

--- src/controllers/Error.php
+++ src/controllers/Error.php
@@ -10,13 +10,54 @@ class Error
 {
     use Controller;
 
-    public function index(string $error_message = ''): void
+    private array $view_data;
+
+    public function __construct()
     {
+        $this->view_data['error_message'] = '500 - Internal server error';
+        $this->view_data['extended_error_message'] = 'Something bad happened';
+    }
+
+    public function handleDatabaseError(): void
+    {
+        $this->view_data['extended_error_message'] = 'Unable to connect to database.';
+
         $this->view(
             'Error',
+            $this->view_data,
             template_title: "Error",
-            template_meta_description: $error_message,
             enableIndexing: false
         );
+    }
+
+    public function handlePageNotFoundError($extended_error_message = 'Ensure that you have properly typed the URL.'
+    ): void {
+        $this->view_data['error_message'] = '404 - Page not found';
+        $this->view_data['extended_error_message'] = $extended_error_message;
+
+        $this->view(
+            'Error',
+            $this->view_data,
+            template_title: "Page not found",
+            enableIndexing: false
+        );
+    }
+
+    public function handleUnknownError(): void
+    {
+        $this->view_data['extended_error_message'] = 'The server has encountered a situation 
+        it does not know how to handle.';
+
+        $this->view(
+            'Error',
+            $this->view_data,
+            template_title: "Unknown error",
+            enableIndexing: false
+        );
+    }
+
+    public function index(): void
+    {
+        $this->handlePageNotFoundError();
     }
 }
