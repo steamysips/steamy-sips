d31f3264dbf4aed62a9704ec789d1c32368a25c0
creme332 <65414576+creme332@users.noreply.github.com>
2024-05-09 11:18:16 +0400

    improve UI

--- src/views/Contact.php
+++ src/views/Contact.php
@@ -14,36 +14,36 @@ declare(strict_types=1);
 
 ?>
 
-<main>
+<main class="container">
     <div>
         <h1>Contact Us</h1>
         <?php if (isset($_GET['success']) && $_GET['success'] === 'true') : ?>
             <p class="success-message">Your message has been sent successfully!</p>
         <?php endif; ?>
         <form action="" method="post">
-            <label for="firstname">First name:</label><br>
+            <label for="firstname">First name</label>
             <input type="text" id="firstname" name="first_name" value="<?= htmlspecialchars($defaultFirstName) ?>" required>
             <?php if (isset($errors['first_name'])) : ?>
-                <span class="warning"><?= $errors['first_name'] ?></span>
-            <?php endif; ?><br><br>
+                <small class="warning"><?= $errors['first_name'] ?></small>
+            <?php endif; ?>
 
-            <label for="lastname">Last name:</label><br>
+            <label for="lastname">Last name</label>
             <input type="text" id="lastname" name="last_name" value="<?= htmlspecialchars($defaultLastName) ?>" required>
             <?php if (isset($errors['last_name'])) : ?>
-                <span class="warning"><?= $errors['last_name'] ?></span>
-            <?php endif; ?><br><br>
+                <small class="warning"><?= $errors['last_name'] ?></small>
+            <?php endif; ?>
 
-            <label for="email">Email address:</label><br>
+            <label for="email">Your email</label>
             <input type="email" id="email" name="email" value="<?= htmlspecialchars($defaultEmail) ?>" required>
             <?php if (isset($errors['email'])) : ?>
-                <span class="warning"><?= $errors['email'] ?></span>
-            <?php endif; ?><br><br>
+                <small class="warning"><?= $errors['email'] ?></small>
+            <?php endif; ?>
 
-            <label for="message">Details:</label><br>
+            <label for="message">Your message</label>
             <textarea id="message" name="message" rows="5" cols="30" required><?= htmlspecialchars($defaultMessage) ?></textarea>
             <?php if (isset($errors['message'])) : ?>
-                <span class="warning"><?= $errors['message'] ?></span>
-            <?php endif; ?><br><br>
+                <small class="warning"><?= $errors['message'] ?></small>
+            <?php endif; ?>
 
             <input type="submit" name="form_submit" value="Submit">
         </form>
