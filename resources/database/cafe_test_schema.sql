c381b0de74d10e5aa129e848538f00349b2ceb48
creme332 <65414576+creme332@users.noreply.github.com>
2024-05-15 21:14:57 +0400

    create a separate script for creating testing database

---
+++ resources/database/cafe_test_schema.sql
@@ -0,0 +1,292 @@
+-- MySQL dump 10.19  Distrib 10.3.38-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: cafe_test
+-- ------------------------------------------------------
+-- Server version	10.3.38-MariaDB-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `cafe_test`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `cafe_test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `cafe_test`;
+
+--
+-- Table structure for table `administrator`
+--
+
+DROP TABLE IF EXISTS `administrator`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `administrator` (
+                                 `user_id` int(11) unsigned NOT NULL,
+                                 `job_title` varchar(255) NOT NULL,
+                                 `is_super_admin` tinyint(1) DEFAULT 0,
+                                 PRIMARY KEY (`user_id`),
+                                 CONSTRAINT `admin_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+                                 CONSTRAINT `job_title_length` CHECK (char_length(`job_title`) > 3)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client` (
+                          `user_id` int(11) unsigned NOT NULL,
+                          `street` varchar(255) NOT NULL,
+                          `city` varchar(255) NOT NULL,
+                          `district_id` int(11) unsigned DEFAULT NULL,
+                          PRIMARY KEY (`user_id`),
+                          KEY `client_district_district_id_fk` (`district_id`),
+                          CONSTRAINT `client_district_district_id_fk` FOREIGN KEY (`district_id`) REFERENCES `district` (`district_id`) ON UPDATE CASCADE,
+                          CONSTRAINT `client_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+                          CONSTRAINT `client_city_length` CHECK (char_length(`city`) > 2),
+                          CONSTRAINT `client_street_length` CHECK (char_length(`street`) > 3)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+                           `comment_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                           `text` varchar(2000) NOT NULL,
+                           `created_date` datetime NOT NULL DEFAULT current_timestamp(),
+                           `parent_comment_id` int(10) unsigned DEFAULT NULL,
+                           `user_id` int(10) unsigned NOT NULL,
+                           `review_id` int(10) unsigned DEFAULT NULL,
+                           PRIMARY KEY (`comment_id`),
+                           KEY `comment_comment_comment_id_fk` (`parent_comment_id`),
+                           KEY `comment_user_user_id_fk` (`user_id`),
+                           KEY `comment_review_review_id_fk` (`review_id`),
+                           CONSTRAINT `comment_comment_comment_id_fk` FOREIGN KEY (`parent_comment_id`) REFERENCES `comment` (`comment_id`),
+                           CONSTRAINT `comment_review_review_id_fk` FOREIGN KEY (`review_id`) REFERENCES `review` (`review_id`),
+                           CONSTRAINT `comment_user_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `district`
+--
+
+DROP TABLE IF EXISTS `district`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `district` (
+                            `district_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                            `name` varchar(30) NOT NULL,
+                            PRIMARY KEY (`district_id`),
+                            UNIQUE KEY `name` (`name`),
+                            CONSTRAINT `name_values` CHECK (`name` in ('Moka','Port Louis','Flacq','Curepipe','Black River','Savanne','Grand Port','Riviere du Rempart','Pamplemousses','Mahebourg','Plaines Wilhems'))
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order` (
+                         `order_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                         `status` varchar(20) DEFAULT 'pending',
+                         `created_date` datetime DEFAULT current_timestamp(),
+                         `pickup_date` datetime DEFAULT NULL,
+                         `client_id` int(11) unsigned DEFAULT NULL,
+                         `store_id` int(10) unsigned DEFAULT NULL,
+                         PRIMARY KEY (`order_id`),
+                         KEY `order_fk` (`client_id`),
+                         KEY `order_store_store_id_fk` (`store_id`),
+                         CONSTRAINT `order_fk` FOREIGN KEY (`client_id`) REFERENCES `client` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+                         CONSTRAINT `order_store_store_id_fk` FOREIGN KEY (`store_id`) REFERENCES `store` (`store_id`),
+                         CONSTRAINT `pickup_date_range` CHECK (`pickup_date` is null or `pickup_date` >= `created_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `order_product`
+--
+
+DROP TABLE IF EXISTS `order_product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_product` (
+                                 `order_id` int(11) unsigned NOT NULL,
+                                 `product_id` int(11) unsigned NOT NULL,
+                                 `cup_size` varchar(20) NOT NULL,
+                                 `milk_type` varchar(20) NOT NULL,
+                                 `quantity` int(11) unsigned DEFAULT NULL,
+                                 `unit_price` decimal(10,2) DEFAULT NULL,
+                                 PRIMARY KEY (`order_id`,`product_id`,`cup_size`,`milk_type`),
+                                 KEY `order_product_product_product_id_fk` (`product_id`),
+                                 CONSTRAINT `order_product_order_order_id_fk` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`),
+                                 CONSTRAINT `order_product_product_product_id_fk` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`),
+                                 CONSTRAINT `cup_size` CHECK (`cup_size` in ('small','medium','large')),
+                                 CONSTRAINT `milk_type` CHECK (`milk_type` in ('almond','coconut','oat','soy')),
+                                 CONSTRAINT `quantity_range` CHECK (`quantity` > 0),
+                                 CONSTRAINT `unit_price_range` CHECK (`unit_price` > 0)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `password_change_request`
+--
+
+DROP TABLE IF EXISTS `password_change_request`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `password_change_request` (
+                                           `request_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                                           `user_id` int(11) unsigned NOT NULL,
+                                           `token_hash` varchar(255) NOT NULL,
+                                           `expiry_date` datetime NOT NULL,
+                                           `used` tinyint(1) NOT NULL DEFAULT 0,
+                                           PRIMARY KEY (`request_id`),
+                                           KEY `request_fk` (`user_id`),
+                                           CONSTRAINT `request_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+                           `product_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                           `name` varchar(255) NOT NULL,
+                           `calories` int(11) unsigned DEFAULT NULL CHECK (`calories` >= 0),
+                           `img_url` varchar(255) NOT NULL,
+                           `img_alt_text` varchar(150) NOT NULL,
+                           `category` varchar(50) NOT NULL,
+                           `price` decimal(10,2) NOT NULL,
+                           `description` text DEFAULT NULL CHECK (char_length(`description`) > 0),
+                           `created_date` datetime NOT NULL DEFAULT current_timestamp(),
+                           PRIMARY KEY (`product_id`),
+                           CONSTRAINT `name_length` CHECK (char_length(`name`) > 2),
+                           CONSTRAINT `img_alt_text_length` CHECK (char_length(`img_alt_text`) between 5 and 150),
+                           CONSTRAINT `category_length` CHECK (char_length(`category`) > 2),
+                           CONSTRAINT `img_url_format` CHECK (`img_url` like '%.png' or `img_url` like '%.jpeg' or `img_url` like '%.avif' or `img_url` like '%.jpg' or `img_url` like '%.webp')
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `review` (
+                          `review_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                          `rating` int(11) unsigned NOT NULL,
+                          `created_date` datetime NOT NULL DEFAULT current_timestamp(),
+                          `text` varchar(2000) NOT NULL,
+                          `client_id` int(11) unsigned DEFAULT NULL,
+                          `product_id` int(11) unsigned DEFAULT NULL,
+                          PRIMARY KEY (`review_id`),
+                          KEY `review_1fk` (`client_id`),
+                          KEY `review_2fk` (`product_id`),
+                          CONSTRAINT `review_1fk` FOREIGN KEY (`client_id`) REFERENCES `client` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+                          CONSTRAINT `review_2fk` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+                          CONSTRAINT `check_rating` CHECK (`rating` between 1 and 5),
+                          CONSTRAINT `text_length` CHECK (char_length(`text`) >= 2)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `store`
+--
+
+DROP TABLE IF EXISTS `store`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `store` (
+                         `store_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                         `phone_no` varchar(255) NOT NULL,
+                         `street` varchar(255) NOT NULL,
+                         `coordinate` point NOT NULL,
+                         `district_id` int(10) unsigned NOT NULL,
+                         `city` varchar(255) NOT NULL,
+                         PRIMARY KEY (`store_id`),
+                         KEY `store_district_district_id_fk` (`district_id`),
+                         SPATIAL KEY `store_coordinate_index` (`coordinate`),
+                         CONSTRAINT `store_district_district_id_fk` FOREIGN KEY (`district_id`) REFERENCES `district` (`district_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `store_product`
+--
+
+DROP TABLE IF EXISTS `store_product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `store_product` (
+                                 `store_id` int(11) unsigned NOT NULL,
+                                 `product_id` int(11) unsigned NOT NULL,
+                                 `stock_level` int(10) unsigned NOT NULL DEFAULT 0,
+                                 PRIMARY KEY (`store_id`,`product_id`),
+                                 KEY `store_product_product_product_id_fk` (`product_id`),
+                                 CONSTRAINT `store_product_product_product_id_fk` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`),
+                                 CONSTRAINT `store_product_store_store_id_fk` FOREIGN KEY (`store_id`) REFERENCES `store` (`store_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+                        `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                        `email` varchar(320) NOT NULL,
+                        `first_name` varchar(255) DEFAULT NULL,
+                        `password` varchar(255) DEFAULT NULL,
+                        `phone_no` varchar(255) NOT NULL,
+                        `last_name` varchar(255) DEFAULT NULL,
+                        PRIMARY KEY (`user_id`),
+                        UNIQUE KEY `unique_email` (`email`),
+                        CONSTRAINT `email_format` CHECK (`email` like '%@%.%'),
+                        CONSTRAINT `password_length` CHECK (char_length(`password`) > 8),
+                        CONSTRAINT `phone_number_length` CHECK (char_length(`phone_no`) > 6),
+                        CONSTRAINT `first_name_length` CHECK (char_length(`first_name`) > 2),
+                        CONSTRAINT `last_name_length` CHECK (char_length(`first_name`) > 2)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-15 20:45:06
