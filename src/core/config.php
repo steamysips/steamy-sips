c9c6dca9ea4413384b2d454d339343989044a23e
creme332 <65414576+creme332@users.noreply.github.com>
2024-05-23 08:11:23 +0400

    use testing database when request contains X-Test-Env in header

--- src/core/config.php
+++ src/core/config.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 // load environment variables
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../..');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../..');
 $dotenv->load();
 
 // define database credentials
@@ -11,7 +11,12 @@ define('DB_HOST', $_ENV['DB_HOST']);
 define('DB_USERNAME', $_ENV['DB_USERNAME']);
 define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
 
-if (defined('PHPUNIT_STEAMY_TESTSUITE') && PHPUNIT_STEAMY_TESTSUITE) {
+
+// Check for a custom header to switch to the testing environment
+if (isset($_SERVER['HTTP_X_TEST_ENV']) && $_SERVER['HTTP_X_TEST_ENV'] === 'testing') {
+    // a request is coming from the testing environment
+    define('DB_NAME', $_ENV['TEST_DB_NAME']);
+} elseif (defined('PHPUNIT_STEAMY_TESTSUITE') && PHPUNIT_STEAMY_TESTSUITE) {
     // application is currently being tested with phpunit => use testing database
     define('DB_NAME', $_ENV['TEST_DB_NAME']);
 } else {
