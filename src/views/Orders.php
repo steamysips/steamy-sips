242c6d6fa485a9bafb9fc132930639673bb970d6
creme332 <65414576+creme332@users.noreply.github.com>
2024-05-30 19:58:44 +0400

    use better variable names, fix bug with htmlspecialchars()

--- src/views/Orders.php
+++ src/views/Orders.php
@@ -3,24 +3,25 @@
 declare(strict_types=1);
 
 /**
- * @var Order $orders 
- * @var OrderProduct $orderproduct 
+ * @var Order $order Current order
+ * @var OrderProduct[] $line_items Line items for current order
  */
 
 use Steamy\Model\Order;
 use Steamy\Model\OrderProduct;
+
 ?>
 
 <main class="container">
-    <h1>Order #<?= filter_var($orders->getOrderID(), FILTER_SANITIZE_NUMBER_INT); ?></h1>
+    <h1>Order #<?= filter_var($order->getOrderID(), FILTER_SANITIZE_NUMBER_INT); ?></h1>
     <section>
         <h2>Order Details</h2>
-        <p><strong>Order ID:</strong> <?= filter_var($orders->getOrderID(), FILTER_SANITIZE_NUMBER_INT); ?></p>
-        <p><strong>Date:</strong> <?= htmlspecialchars($orders->getCreatedDate()->format('Y-m-d H:i:s')) ?></p>
-        <p><strong>Status:</strong> <?= htmlspecialchars(ucfirst($orders->getStatus()->value)) ?></p>
-        <p><strong>Total Price:</strong> $<?= htmlspecialchars(number_format($orders->calculateTotalPrice(), 2)) ?></p>
+        <p><strong>Order ID:</strong> <?= filter_var($order->getOrderID(), FILTER_SANITIZE_NUMBER_INT); ?></p>
+        <p><strong>Date:</strong> <?= htmlspecialchars($order->getCreatedDate()->format('Y-m-d H:i:s')) ?></p>
+        <p><strong>Status:</strong> <?= htmlspecialchars(ucfirst($order->getStatus()->value)) ?></p>
+        <p><strong>Total Price:</strong> $<?= htmlspecialchars(number_format($order->calculateTotalPrice(), 2)) ?></p>
     </section>
-    
+
     <section>
         <h2>Order Items</h2>
         <table>
@@ -29,18 +30,19 @@ use Steamy\Model\OrderProduct;
                 <th>Quantity</th>
                 <th>Milk Type</th>
                 <th>Cup Size</th>
-                <th>Price</th>
+                <th>Unit Price</th>
             </tr>
             <?php
-            foreach ($orderproduct as $item): ?>
+            foreach ($line_items as $item): ?>
                 <tr>
                     <td><?= htmlspecialchars($item->getProductName()) ?></td>
-                    <td><?= htmlspecialchars($item->getQuantity()) ?></td>
+                    <td><?= filter_var($item->getQuantity(), FILTER_SANITIZE_NUMBER_INT) ?></td>
                     <td><?= htmlspecialchars($item->getMilkType()) ?></td>
                     <td><?= htmlspecialchars($item->getCupSize()) ?></td>
-                    <td>$<?= htmlspecialchars(number_format($item->getPrice(), 2)) ?></td>
+                    <td>$<?= (number_format($item->getUnitPrice(), 2)) ?></td>
                 </tr>
-            <?php endforeach; ?>
+            <?php
+            endforeach; ?>
         </table>
     </section>
 </main>
