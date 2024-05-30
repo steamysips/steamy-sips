401db2bb77f28ff9c8a618d2462941ca0d98b7d8
creme332 <65414576+creme332@users.noreply.github.com>
2024-05-30 19:59:19 +0400

    format file, update view data variable names, fetch line items appropriately

--- src/controllers/Orders.php
+++ src/controllers/Orders.php
@@ -17,27 +17,25 @@ class Orders
 
     private function validateURL(): bool
     {
-    $url = Utility::getURL();
-    $parts = explode('/', $url);
-    // Check if the URL matches the expected pattern
-    $isValidURL = preg_match('/^orders\/\d+$/', $url) === 1;
-    return $isValidURL;
+        $url = Utility::getURL();
+        // Check if the URL matches the expected pattern
+        return preg_match('/^orders\/\d+$/', $url) === 1;
     }
 
     private function getOrderIDFromURL(): ?int
     {
-    if ($this->validateURL()) {
-        $url = Utility::getURL();
-        $parts = explode('/', $url);
-        // Check if the last part of the URL is a valid integer
-        $lastPart = end($parts);
-        if (is_numeric($lastPart)) {
-            return (int)$lastPart;
-        } else {
-            return null;
+        if ($this->validateURL()) {
+            $url = Utility::getURL();
+            $parts = explode('/', $url);
+            // Check if the last part of the URL is a valid integer
+            $lastPart = end($parts);
+            if (is_numeric($lastPart)) {
+                return (int)$lastPart;
+            } else {
+                return null;
+            }
         }
-    }
-    return null;
+        return null;
     }
 
 
@@ -56,24 +54,24 @@ class Orders
         $order_id = $this->getOrderIDFromURL();
         if ($order_id === null) {
             (new Error())->handlePageNotFoundError();
-            die();
+            return;
         }
 
         $order = Order::getByID($order_id);
         if (!$order) {
             (new Error())->handlePageNotFoundError();
-            die();
+            return;
         }
 
-        $order_products = OrderProduct::getByID($order_id);
+        $order_products = Order::getOrderProducts($order->getOrderID());
 
-        $this->view_data['orders'] = $order;
-        $this->view_data['orderproduct'] = $order_products;
+        $this->view_data['order'] = $order;
+        $this->view_data['line_items'] = $order_products;
 
         $this->view(
             'orders',
             $this->view_data,
-            'Order',
+            'Order #' . $order_id,
             enableIndexing: false
         );
     }
